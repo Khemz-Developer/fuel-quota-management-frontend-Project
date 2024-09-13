77a7e269fc9b7126b6b6b68e54d47b4db7c2e116
Khemz-Developer <jalithapramod@gmail.com>
2024-09-13 09:17:33 +0530

    feat(vehicle-registration-portal): initial setup for vehicle-registration-portal

---
+++ Vehicle-Registration-Portal/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <>
+        <h1 className="text-2xl bg-green">
+          Vehicle Registration Portal Systems
+        </h1>
+      </>
+    </>
+  );
+}
+
+export default App;
