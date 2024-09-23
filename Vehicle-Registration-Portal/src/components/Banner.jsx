7a636b53ae00d3da56aa02f35befee79acc0bcba
Khemz-Developer <jalithapramod@gmail.com>
2024-09-23 15:16:00 +0530

    feat(vehicle-registration-portal): minor changes in Banner

--- Vehicle-Registration-Portal/src/components/Banner.jsx
+++ Vehicle-Registration-Portal/src/components/Banner.jsx
@@ -6,7 +6,7 @@
 //           <div className="ml-10"> {/* Increase width and move right */}
 //             <img src="/images/home/2.png" alt="" className="w-full transition-all duration-500 animate-zoomIn hover:rotate-1" /> {/* Make image take full width */}
 //           </div>
-  
+
 //           {/* Text */}
 //           <div className="px-4 space-y-6 md:w-1/2">
 //             <h2 className="text-4xl font-bold leading-snug md:leading-snug md:text-5xl">
@@ -24,7 +24,7 @@
 //       </div>
 //     );
 //   };
-  
+
 //   export default Banner;
 const Banner = () => {
   return (
@@ -32,9 +32,9 @@ const Banner = () => {
       <div className="relative flex items-center justify-between gap-0 py-24 md:flex-row-reverse">
         {/* Decorative background element */}
         <div className="absolute w-[300px] h-[300px] bg-green rounded-full opacity-20 right-0 top-1/4 -z-10 animate-pulse"></div>
-        
+
         {/* Image */}
-        <div className="ml-10 transition-transform duration-300 ease-out transform md:w-1/2 hover:scale-105">
+        <div className="hidden ml-10 transition-transform duration-300 ease-out transform md:w-1/2 hover:scale-105 md:block">
           <img
             src="/images/home/banner1.png"
             alt="Vehicle Registration"
@@ -45,14 +45,14 @@ const Banner = () => {
         {/* Text */}
         <div className="px-4 space-y-8 md:w-1/2">
           <h2 className="text-3xl font-extrabold leading-snug md:leading-snug md:text-6xl">
-            Manage Your Fuel Quota with {" "}
-            <span className="text-green">Ease</span>
+            Manage Your Fuel Quota with <span className="text-green">Ease</span>
           </h2>
           <p className="max-w-md text-xl text-gray-600">
-            Register your vehicle and track your fuel usage with our convenient online portal.
+            Register your vehicle and track your fuel usage with our convenient
+            online portal.
           </p>
 
-          <div className="flex space-x-4">
+          <div className="flex hidden space-x-4 md:block">
             <button className="px-8 py-2 font-semibold text-white transition duration-300 rounded-full shadow-lg bg-green hover:bg-green-700 hover:shadow-xl">
               Manage Now
             </button>
