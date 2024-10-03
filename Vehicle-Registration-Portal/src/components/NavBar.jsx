f50a2c0689b3694ca3feeb921fc0eb4dca71afc0
Khemz-Developer <jalithapramod@gmail.com>
2024-10-03 20:40:04 +0530

    feat(vehicle-registration-portal): add motion for vehical registration form

--- Vehicle-Registration-Portal/src/components/NavBar.jsx
+++ Vehicle-Registration-Portal/src/components/NavBar.jsx
@@ -1,11 +1,120 @@
+// import { FaRegUser } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import Modal from "./SignInModal";
+
+// function NavBar() {
+//   const [isSticky, setSticky] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const offset = window.scrollY;
+//       if (offset > 0) {
+//         setSticky(true);
+//       } else {
+//         setSticky(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   const navItems = (
+//     <>
+//       <li>
+//         <a className="text-black" href="/">
+//           Home
+//         </a>
+//       </li>
+//       <li>
+//         <a className="text-black" href="/registration">
+//           Register Vehicle
+//         </a>
+//       </li>
+//       <li>
+//         <a className="text-black" href="/dashboard">
+//           Dashboard
+//         </a>
+//       </li>
+//       <li>
+//         <a className="text-black" href="/help">
+//           Help & Support
+//         </a>
+//       </li>
+//     </>
+//   );
+
+//   return (
+//     <header
+//       className={`container mx-auto max-w-screen-2xl ${
+//         isSticky ? "sticky top-0 bg-white z-10 shadow" : ""
+//       }`}
+//     >
+//       <div className="navbar xl:px-24">
+//         <div className="navbar-start">
+//           <div className="dropdown">
+//             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 className="w-5 h-5"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 stroke="currentColor"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth="2"
+//                   d="M4 6h16M4 12h8m-8 6h16"
+//                 />
+//               </svg>
+//             </div>
+//             <ul
+//               tabIndex={0}
+//               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+//             >
+//               {navItems}
+//             </ul>
+//           </div>
+//           <a href="/">
+//             <p className="text-bold" style={{ width: "200px" }}>
+//               <span className="text-md text-green">Vehicle Registration</span> Portal
+//             </p>
+//           </a>
+//         </div>
+//         <div className="hidden navbar-center lg:flex">
+//           <ul className="px-1 menu menu-horizontal">{navItems}</ul>
+//         </div>
+//         <div className="navbar-end">
+//           <button
+//             onClick={() => document.getElementById("my_modal_5").showModal()}
+//             className="flex items-center gap-2 px-6 text-white rounded-full btn bg-green"
+//           >
+//             <FaRegUser />
+//             Login / Register
+//           </button>
+//           <Modal />
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default NavBar;
 import { FaRegUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Modal from "./SignInModal";
 
+
 function NavBar() {
   const [isSticky, setSticky] = useState(false);
-
+  const location = useLocation(); // Get current path
+  
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
@@ -23,27 +132,42 @@ function NavBar() {
     };
   }, []);
 
+  // Function to check if the current path matches the link
+  const isActive = (path) => location.pathname === path;
+
   const navItems = (
     <>
       <li>
-        <a className="text-black" href="/">
+        <Link
+          to="/"
+          className={`text-black ${isActive("/") ? "text-[#176B87]" : ""}`}
+        >
           Home
-        </a>
+        </Link>
       </li>
       <li>
-        <a className="text-black" href="/registration">
+        <Link
+          to="/registration"
+          className={`text-black ${isActive("/registration") ? "text-[#176B87]" : ""}`}
+        >
           Register Vehicle
-        </a>
+        </Link>
       </li>
       <li>
-        <a className="text-black" href="/dashboard">
+        <Link
+          to="/dashboard"
+          className={`text-black ${isActive("/dashboard") ? "text-[#176B87]" : ""}`}
+        >
           Dashboard
-        </a>
+        </Link>
       </li>
       <li>
-        <a className="text-black" href="/help">
+        <Link
+          to="/help"
+          className={`text-black ${isActive("/help") ? "text-[#176B87]" : ""}`}
+        >
           Help & Support
-        </a>
+        </Link>
       </li>
     </>
   );
@@ -82,7 +206,8 @@ function NavBar() {
           </div>
           <a href="/">
             <p className="text-bold" style={{ width: "200px" }}>
-              <span className="text-md text-green">Vehicle Registration</span> Portal
+              <span className="text-md text-green">Vehicle Registration</span>{" "}
+              Portal
             </p>
           </a>
         </div>
--- Vehicle-Registration-Portal/src/pages/RegistrationPage.jsx
+++ Vehicle-Registration-Portal/src/pages/RegistrationPage.jsx
@@ -1,5 +1,199 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+// import { useState } from "react";
+
+
+// function RegistrationPage() {
+//   const [formData, setFormData] = useState({
+//     registrationNumber: "",
+//     ownerName: "",
+//     engineNumber: "",
+//     vehicleClass: "",
+//     conditionsAndNotes: "",
+//     make: "",
+//     model: "",
+//     selectedDate: "",
+//     ownershipName: "",
+//     isMortgaged: false,
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value, type, checked } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: type === "checkbox" ? checked : value,
+//     });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Handle form submission, like sending data to the backend API
+//     console.log(formData);
+//   };
+
+//   return (
+//     <div className="container p-6 mx-auto mt-10 mb-5 bg-white rounded-lg shadow-lg shadow-green-200">
+//       <h4 className="mb-8 text-2xl font-bold text-center text-green">
+//         Vehicle Registration
+//       </h4>
+//       <form onSubmit={handleSubmit} className="space-y-6">
+//         <div className="flex flex-row justify-between w-full">
+//           <div className="flex-1 mr-4">
+//             <label className="block mb-2 text-sm font-medium text-green">
+//               Owner Name
+//             </label>
+//             <input
+//               type="text"
+//               name="ownerName"
+//               placeholder="Owner's Name"
+//               value={formData.ownerName}
+//               onChange={handleChange}
+//               className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-200 focus:border-custom placeholder-custom"
+//             />
+//           </div>
+
+//           <div className="flex-1 ml-4">
+//             <label className="block mb-2 text-sm font-medium text-green">
+//               Registration Number
+//             </label>
+//             <input
+//               type="text"
+//               name="registrationNumber"
+//               placeholder="Registration Number"
+//               value={formData.registrationNumber}
+//               onChange={handleChange}
+//               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-200 focus:border-green"
+//             />
+//           </div>
+//         </div>
+
+//         <div className="flex flex-row justify-between w-full">
+//           <div className="flex-1 mr-4">
+//             <label className="block mb-2 text-sm font-medium text-green">
+//               Engine Number
+//             </label>
+//             <input
+//               type="text"
+//               name="engineNumber"
+//               placeholder="Engine Number"
+//               value={formData.engineNumber}
+//               onChange={handleChange}
+//               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//             />
+//           </div>
+
+//           <div className="flex-1 ml-4">
+//             <label className="block mb-2 text-sm font-medium text-green">
+//               Vehicle Class
+//             </label>
+//             <input
+//               type="text"
+//               name="vehicleClass"
+//               placeholder="Vehicle Class"
+//               value={formData.vehicleClass}
+//               onChange={handleChange}
+//               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//             />
+//           </div>
+//         </div>
+
+//         <div className="flex flex-row justify-between w-full">
+//           <div className="flex-1 mr-4">
+//             <label className="block mb-2 text-sm font-medium text-green">
+//               Model
+//             </label>
+//             <input
+//               type="text"
+//               name="model"
+//               placeholder="Vehicle Model"
+//               value={formData.model}
+//               onChange={handleChange}
+//               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//             />
+//           </div>
+
+//           <div className="flex-1 ml-4">
+//             <label className="block mb-2 text-sm font-medium text-green">
+//               Year of Manufacture
+//             </label>
+//             <input
+//               type="date"
+//               name="yearOfManufacture"
+//               value={formData.yearOfManufacture}
+//               onChange={handleChange}
+//               className="w-full px-4 py-2 border border-green-500 rounded-lg shadow-sm text-placeholder placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//             />
+//           </div>
+//         </div>
+
+//         <div>
+//           <label className="block mb-2 text-sm font-medium text-green">
+//             Conditions and Notes
+//           </label>
+//           <textarea
+//             name="conditionsAndNotes"
+//             placeholder="Enter any conditions or notes"
+//             value={formData.conditionsAndNotes}
+//             onChange={handleChange}
+//             className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//           />
+//         </div>
+
+//         <div>
+//           <label className="block mb-2 text-sm font-medium text-green">
+//             Make
+//           </label>
+//           <input
+//             type="text"
+//             name="make"
+//             placeholder="Vehicle Make"
+//             value={formData.make}
+//             onChange={handleChange}
+//             className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//           />
+//         </div>
+
+//         <div>
+//           <label className="block mb-2 text-sm font-medium text-green">
+//             Ownership Name
+//           </label>
+//           <input
+//             type="text"
+//             name="ownershipName"
+//             placeholder="Owner's Name"
+//             value={formData.ownershipName}
+//             onChange={handleChange}
+//             className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+//           />
+//         </div>
+
+//         <div className="flex items-center">
+//           <input
+//             type="checkbox"
+//             name="isMortgaged"
+//             checked={formData.isMortgaged}
+//             onChange={handleChange}
+//             className="w-4 h-4 text-green-500 border-gray-300 rounded placeholder-custom focus:ring-green-500"
+//           />
+//           <label className="block ml-2 text-sm font-medium text-green">
+//             Is Mortgaged
+//           </label>
+//         </div>
+
+//         <div className="flex justify-center">
+//           <button
+//             type="submit"
+//             className="px-8 py-2 font-semibold transition duration-300 border-2 rounded-full w-80 text-green border-green hover:text-white hover:bg-green"
+//           >
+//             Register Vehicle
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default RegistrationPage;
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 function RegistrationPage() {
   const [formData, setFormData] = useState({
@@ -25,18 +219,29 @@ function RegistrationPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission, like sending data to the backend API
+    // Handle form submission
     console.log(formData);
   };
 
   return (
     <div className="container p-6 mx-auto mt-10 mb-5 bg-white rounded-lg shadow-lg shadow-green-200">
-      <h4 className="mb-8 text-2xl font-bold text-center text-green">
+      <motion.h4
+        className="mb-8 text-2xl font-bold text-center text-green"
+        initial={{ opacity: 0, scale: 0.5 }} // start state (invisible and scaled down)
+        animate={{ opacity: 1, scale: 1 }}    // end state (fully visible and normal scale)
+        transition={{ duration: 0.5 }}         // animation duration
+      >
         Vehicle Registration
-      </h4>
+      </motion.h4>
       <form onSubmit={handleSubmit} className="space-y-6">
-        <div className="flex flex-row justify-between w-full">
-          <div className="flex-1 mr-4">
+        {/* First row: Owner Name and Registration Number */}
+        <div className="flex flex-wrap -mx-4">
+          <motion.div
+            className="w-full px-4 mb-4 md:w-1/2 md:mb-0"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             <label className="block mb-2 text-sm font-medium text-green">
               Owner Name
             </label>
@@ -48,9 +253,14 @@ function RegistrationPage() {
               onChange={handleChange}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-200 focus:border-custom placeholder-custom"
             />
-          </div>
+          </motion.div>
 
-          <div className="flex-1 ml-4">
+          <motion.div
+            className="w-full px-4 md:w-1/2"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             <label className="block mb-2 text-sm font-medium text-green">
               Registration Number
             </label>
@@ -62,11 +272,16 @@ function RegistrationPage() {
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-200 focus:border-green"
             />
-          </div>
+          </motion.div>
         </div>
 
-        <div className="flex flex-row justify-between w-full">
-          <div className="flex-1 mr-4">
+        {/* Second row: Engine Number and Vehicle Class */}
+        <div className="flex flex-wrap -mx-4">
+          <motion.div 
+          className="w-full px-4 mb-4 md:w-1/2 md:mb-0"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}>
             <label className="block mb-2 text-sm font-medium text-green">
               Engine Number
             </label>
@@ -78,9 +293,12 @@ function RegistrationPage() {
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
             />
-          </div>
+          </motion.div>
 
-          <div className="flex-1 ml-4">
+          <motion.div className="w-full px-4 mb-4 md:w-1/2 md:mb-0"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}>
             <label className="block mb-2 text-sm font-medium text-green">
               Vehicle Class
             </label>
@@ -92,11 +310,15 @@ function RegistrationPage() {
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
             />
-          </div>
+          </motion.div>
         </div>
 
-        <div className="flex flex-row justify-between w-full">
-          <div className="flex-1 mr-4">
+        {/* Third row: Model and Year of Manufacture */}
+        <div className="flex flex-wrap -mx-4">
+          <motion.div className="w-full px-4 mb-4 md:w-1/2 md:mb-0"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}>
             <label className="block mb-2 text-sm font-medium text-green">
               Model
             </label>
@@ -108,9 +330,12 @@ function RegistrationPage() {
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
             />
-          </div>
+          </motion.div>
 
-          <div className="flex-1 ml-4">
+          <motion.div className="w-full px-4 md:w-1/2"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}>
             <label className="block mb-2 text-sm font-medium text-green">
               Year of Manufacture
             </label>
@@ -121,10 +346,14 @@ function RegistrationPage() {
               onChange={handleChange}
               className="w-full px-4 py-2 border border-green-500 rounded-lg shadow-sm text-placeholder placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
             />
-          </div>
+          </motion.div>
         </div>
 
-        <div>
+        {/* Conditions and Notes */}
+        <motion.div 
+          initial={{ opacity: 0, scale: 0.5 }} 
+          animate={{ opacity: 1, scale: 1 }} 
+          transition={{ duration: 0.5 }}>
           <label className="block mb-2 text-sm font-medium text-green">
             Conditions and Notes
           </label>
@@ -135,36 +364,46 @@ function RegistrationPage() {
             onChange={handleChange}
             className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
           />
+        </motion.div>
+
+        {/* Make and Ownership Name */}
+        <div className="flex flex-wrap -mx-4">
+          <motion.div className="w-full px-4 mb-4 md:w-1/2 md:mb-0"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}>
+            <label className="block mb-2 text-sm font-medium text-green">
+              Make
+            </label>
+            <input
+              type="text"
+              name="make"
+              placeholder="Vehicle Make"
+              value={formData.make}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </motion.div>
+
+          <motion.div className="w-full px-4 md:w-1/2"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}>
+            <label className="block mb-2 text-sm font-medium text-green">
+              Ownership Name
+            </label>
+            <input
+              type="text"
+              name="ownershipName"
+              placeholder="Owner's Name"
+              value={formData.ownershipName}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </motion.div>
         </div>
 
-        <div>
-          <label className="block mb-2 text-sm font-medium text-green">
-            Make
-          </label>
-          <input
-            type="text"
-            name="make"
-            placeholder="Vehicle Make"
-            value={formData.make}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-        </div>
-
-        <div>
-          <label className="block mb-2 text-sm font-medium text-green">
-            Ownership Name
-          </label>
-          <input
-            type="text"
-            name="ownershipName"
-            placeholder="Owner's Name"
-            value={formData.ownershipName}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-lg shadow-sm placeholder-custom focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-        </div>
-
+        {/* Is Mortgaged */}
         <div className="flex items-center">
           <input
             type="checkbox"
@@ -173,8 +412,8 @@ function RegistrationPage() {
             onChange={handleChange}
             className="w-4 h-4 text-green-500 border-gray-300 rounded placeholder-custom focus:ring-green-500"
           />
-          <label className="block ml-2 text-sm font-medium text-green">
-            Is Mortgaged
+          <label className="ml-2 text-sm text-gray-600">
+            Is this vehicle mortgaged?
           </label>
         </div>
 
