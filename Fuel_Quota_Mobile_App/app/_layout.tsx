93ebc1ae1822f183e30fe02093832d739bbfb271
Khemz-Developer <jalithapramod@gmail.com>
2024-10-20 01:23:36 +0530

    feat(fuel-quota-mobile-app): implement Profile Section , Bottom Tab Navigation

--- Fuel_Quota_Mobile_App/app/_layout.tsx
+++ Fuel_Quota_Mobile_App/app/_layout.tsx
@@ -1,11 +1,18 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf'),
+  })
   return (
     <Stack>
       <Stack.Screen name="index" options={{headerShown:false}}/>
       <Stack.Screen name="(auth)" options={{headerShown:false}} />
       <Stack.Screen name="(user)" options={{ headerShown: false }} />
+      <Stack.Screen name="(startup)" options={{ headerShown: false }} />
     </Stack>
   );
 }
